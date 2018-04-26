0706302fdd729401238c2a9415ffd4b5bc7004da
Abhilekha <abhi.java2014@gmail.com>
2018-04-26 16:11:28 +0530

    First Commit

---
+++ src/java/string/examples/DuplicateString.java
@@ -0,0 +1,31 @@
+package java.string.examples;
+
+public class DuplicateString {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		
+		
+		/*String str = "Alexander the Great III the Great, the King of"
+				+ " Macedonia and conqueror of the Persian Empire is considered"
+				+ " one of the greatest military geniuses of all times. "
+				+ "He was inspiration for later conquerors such as Hannibal the"
+				+ " Carthaginian, the Romans Pompey and Caesar, and Napoleon.  "
+				+ "Alexander the Great was born in 356 BC in Pella, the ancient "
+				+ "capital of Macedonia. He was son of Philip II, King of Macedonia,"
+				+ " and Olympias, the princess of neighboring Epirus.Gordium was a home of the famous so-called Gordian Knot. Alexander the Great knew the legend that said that the man who could untie the ancient knot was destined to rule the entire world. To that date nobody had succeeded in raveling the knot. But the young Macedonian king simply slashed it with his sword and unraveling its ends.Alexander the Great then led the army south across Asia Minor.  Ironically, it is not the Persians but the Greek coastal cities which gave the greatest resistance to the Macedonians.  The Greek commander Memnon and his men considerably slow down the advance of Alexander the Great and many Macedonians died during the long and difficult sieges of the Greek cities of Halicarnassus, Miletus, Mylasa.  But at the end the Macedonian army defeated the enemy and conquered the coast of Asia Minor. Alexander the Great then turned northward to central Asia Minor, to the city of Gordium.";
+		int count =0;
+		
+		String[] split = str.split("Alexander the Great");
+		
+		for (int i = 0; i < split.length; i++) {
+			if(split[i].equals("Alexander the Great")){
+				count++;
+				
+			}
+		}
+		System.out.println("Count of  'Alexander the Great' is "+count);*/
+		
+	}
+
+}
